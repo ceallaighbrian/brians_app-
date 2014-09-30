efd83ff17f94262d461e36fa3f6b87ce202ce063
Brian Kelly <ceallaighbrian@gmail.com>
2014-09-30 12:59:07 +0000

    Make a basic User model

---
+++ test/models/user_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "should be valid" do
+   assert @user.valid? 
+  end 
+  
+  test "name should be present" do 
+    @user.name = "" 
+    assert_not @user.valid? 
+    end
+    
+  test "email should be present" do 
+    @user.email = "" 
+    assert_not @user.valid? 
+  end
+  
+  test "name should not be too long" do 
+    @user.name = "a" * 51 
+    assert_not @user.valid? 
+  end
+  
+  test "email validation should accept valid addresses" do 
+    vaild_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.com]
+    vaild_addresses.each do |vaild_addresses| 
+      @user.email = vaild_addresses
+      assert @user.valid? 
+    end
+  end
+  
+  test "email validation should not accept invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_addresses|
+      @user.email = invalid_addresses
+      assert_not @user.valid?
+    end
+  end
+  
+  test "email addresses should be unique" do 
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase 
+    @user.save 
+    assert_not duplicate_user.valid? 
+  end
+  
+  test "password should ave a minimum lenght" do 
+    @user.password = @user.password_confirmation = "a" *  5
+    assert_not @user.valid? 
+  end
+  
+end
